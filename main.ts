43c76f70181b7933bf5408bfb47de7d989de80be
Solarrixs <ma.xxy11@protonmail.com>
2022-06-29 22:35:19 -0400

    adad

--- main.ts
+++ main.ts
@@ -3,32 +3,20 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 // Remember to rename these classes and interfaces!
 
 export default class NoteHider extends Plugin {
-	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'reveal-notes',
 			name: 'Reveal Notes',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+
+            
 		});
 
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+}
 }
